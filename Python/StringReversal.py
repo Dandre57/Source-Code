c5c9921e03c1e398973b45730536d8d921d85d33
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-09 16:52:34 -0500

    Create StringReversal.py

---
+++ Python/StringReversal.py
@@ -0,0 +1,12 @@
+#This program reverses the string that the user inputs
+
+input = raw_input("Enter a string you would like to reverse: ").lower()
+print(input)
+
+conversion = list(input)
+
+i = 0
+
+while i < conversion.length:
+	print(conversion[len(conversion) - 1])
+	i++
