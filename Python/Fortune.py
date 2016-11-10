47d1702d80fb02ffb67e42c5d0298461ac0470fe
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-10 02:39:18 -0500

    Update Fortune.py

--- Python/Fortune.py
+++ Python/Fortune.py
@@ -1,3 +1,27 @@
 # This program will print a random quote from the given array
 
 from random import randint
+from tkinter import *
+
+# Will hold list of different quotes
+fortune[] = {""}
+
+def randQuote(advice):
+  length = len(advice)
+  rand = randint(0, length) - 1 
+  choice = advice[rand]
+  return choice
+
+def interface():
+  root = tkinter.TK()
+  root.title("Fortune Cookie")
+  root.geometry("500x400")
+ 
+  picture = tkinter.PhotoImage(file="filepath/FortuneCookie.jpeg")
+  cookiePic = tkinter.Label(root, image=picture)
+  cookiePic.pack()
+  
+  butt = tkinter.Button(root, "Click For A Fortune", command=)
+  butt.pack()
+  
+  root.mainloop()
