387e64d115145b9e6d1d94165730352545b5533f
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-12 18:02:48 -0500

    Update Server.py

--- Python/Server/Server.py
+++ Python/Server/Server.py
@@ -9,4 +9,8 @@ sock.bind(host, port)
 
 sock.listen()
 while True:
+  connection, address = sock.accept()
+  print("Got connection from:" + address)
+  connection.send("You're connected")
+  connection.close()
   
