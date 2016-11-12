eab1cd241cc7be9120ace1e9bb99720ac126bfff
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-12 16:34:30 -0500

    Update GuessTheNumber.py
    Added comments

--- Python/GuessTheNumber.py
+++ Python/GuessTheNumber.py
@@ -1,26 +1,31 @@
 # Game where the user guesses a random number between one and ten
 
 from random import randint
-import tkMessageBox
 
+"""
+Creates the random number between 1 and 10
+"""
 def randNumber():
 	number = randint(0, 10)
 	return number
 
+"""
+Takes the users guess and will re-run the program if they are wrong
+"""
 def main():
 	input = raw_input("Guess a number between 1 and 10: ")
+	conversion = int(input)
 
-	if input == randNumber():
-		# create line where a pop up box appears if it is right
-		# create line where program will exit if right
+	if conversion == randNumber():
+		print("You Actually Got It Right! \n")
 
-	elif input > 10:
-		# create a pop up box saying the number is not valid
-		# main()
+	elif conversion > 10 or converion < 0:
+		print"The number you input was out of range. \n")
+		main()
 
 	else:
-		# create a pop up box saying there is an error
-		# main()
-
-
-	 
+		print("There was an error in the program. \n")
+		main()
+		
+# Runs the program	
+main()
