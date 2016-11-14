9db44c6edc3f243a1e615a8986873c5b3bbe02a0
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-13 20:06:34 -0500

    Update DiceRoll.py

--- Python/DiceRoll.py
+++ Python/DiceRoll.py
@@ -1,3 +1,18 @@
+# This program rolls two six sided dice
+
 from random import randint
 
 def rollDice():
+  print("Rolling the dice...")
+  firstDice = randint(1, 6)
+  secondDice = randint(1, 6)
+  print(firstDice + "\t" + secondDice + "\n")
+  
+def main():
+  rollDice()
+  
+  input = raw_input("Would you like to roll again?").lower()
+  while input == "yes":
+    rollDice()
+    
+main()
