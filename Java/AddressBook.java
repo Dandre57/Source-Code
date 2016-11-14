ee4413a1430356715e973bfb6a3136174851e60d
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-14 16:18:41 -0500

    Update AddressBook.java

--- Java/AddressBook.java
+++ Java/AddressBook.java
@@ -1,34 +1,63 @@
 // Address Book
 import java.util.*;
+import java.io.*;
 
 public class AddressBook
 {
+	private Scanner scan = null;
+	
+	public AddressBook
+	{
+		try
+		{
+			Scanner scan = new Scanner(System.in);
+
+			File file = new File("AddressBook.txt");
+			file.createNewFile();
+
+			FileWriter writer = new FileWriter(file);
+
+			//String userName;
+			String contactName, contactNumber;
+			
+			//System.out.print("Insert your name: ");
+			//userName = nextln();
+
+			System.out.println("\nInput your contact's name: ");
+			contactName = scan.nextln();
+
+			System.out.println("Input your contact's phone number: ");
+			contactNumber = scan.nextln();
+		
+			//str = String.format("\n%s Address Book", userName);
+			contact = String.format("Name %s\t Phone Number %s\t ", contactName, contactNumber);
+
+			//writer.write();
+			writer.write(contact);
+			
+		} // End of try statement
+		catch(Exception e)
+		{
+			System.out.println("An error has occured");
+		} 
+	}
+	
 	public static void main(String[] args) throws IOException
 	{
-		Scanner scan = new Scanner(System.in);
-
-		File file = new File("AddressBook.txt");
-		file.createNewFile();
-
-		FileWriter writer = new FileWriter(file);
-
-		String userName, contactName, contactNumber;
-
-		System.out.print("Insert your name: ");
-		userName = nextln();
-
-		str = String.format("\n%s Address Book", userName);
-
-		System.out.println("\nInput your contact's name: ");
-		contactName = nextln();
-
-		System.out.println("Input your contact's phone number: ");
-		contactNumber = nextln();
-		contact = String.format("Name %s\t Phone Number %s\t ", contactName, contactNumber);
-
-		writer.write(str);
-		writer.write(contact);
+		new AddressBook();
 		
+		scan = new Scanner(System.in);
+		String answer;
+		
+		System.out.println("Would you like to input another contact?");
+		answer = nextln();
+		
+		while(answer.toLowerCase() == "yes")
+		{
+			System.out.println("Would you like to input another contact?");
+			answer = nextln();
+			new AddressBook();
+		}
 	} // End of main method
 
 } // End of class
