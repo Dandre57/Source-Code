27ecf61fb676696d1c06a380154c9c49f2b2a6b4
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-20 13:00:01 -0500

    Update Alarm.java

--- Java/Alarm.java
+++ Java/Alarm.java
@@ -1,4 +1,28 @@
+import javax.swing.Timer;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 public class Alarm
 {
-
-}
+  public static void main(String[] args)
+  {
+    String music = ".mp3";
+    
+  }// End of main method
+  
+  public void playMusic(String song)
+  {
+    try
+    {
+      Media media = new Media(song);
+      MediaPlayer player = new MediaPlayer(media);
+      player.play();
+    }
+    catch(Exception e)
+    {
+      System.out.println("Couldn't play the song");
+    }
+    
+  }// End of playMusic method
+  
+}// End of class
