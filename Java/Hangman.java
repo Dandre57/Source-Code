bab3f51f676cc5916f410fef7a2422719b082ee0
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-21 18:40:35 -0500

    Update Hangman.java

--- Java/Hangman.java
+++ Java/Hangman.java
@@ -3,5 +3,55 @@ import java.util.Random;
 
 public class Hangman
 {
+  private String[] array = {"batman", "flash", "superman", "cyborg", 
+                            "wonderwoman" ,"aquaman", "ironman", "hulk", 
+                            "thor", "wolverine", "spiderman"}
+  private int numHeroes;
+  private String hero;
+  private int mistakes = 6;
+  
+  
+  public Hangman()
+  {
+    Scanner scan = new Scanner(System.in);
+    
+    chooseHero();
+    String word = getHero();
+    char[] guess;
+      
+    System.out.println("Welcome to Hangman! \n");
+    System.out.println("This version only contain popular heroes!");
+    System.out.print("Take your guess: ");
+    
+    //guess = 
+  }
+  
+  public void choosenHero()
+  {
+    Random rand = new Random();
+    
+    numHeroes = array.length - 1;
+    hero = array[rand.nextInt(numHeroes)];
+  }
+  
+  public String getHero()
+  {
+    return hero; 
+  }
+  
+  public void mistakes()
+  {
+    
+  }
+  
+  public int getMistakes()
+  {
+    return mistakes;
+  }
+  
+  public static void main(String[] args)
+  {
+    new Hangman();
+  }
 
 }
