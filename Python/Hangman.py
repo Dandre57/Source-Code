ed90b4d75cfbbba145ce729ee61cd5238af9aceb
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-21 15:27:00 -0500

    Create Hangman.py

---
+++ Python/Hangman.py
@@ -0,0 +1,9 @@
+from random import randint
+
+heroes = ["batman", "flash", "superman", 
+        "cyborg", "wonderwoman" ,"aquaman",
+        "ironman", "hulk", "thor",
+        "wolverine", "spiderman"]
+
+length = len(heroes)
+rand = randint(0, length)
