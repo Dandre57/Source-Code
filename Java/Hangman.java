4d880f2c383ead584302dba0389c797ba0a6eeb3
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-28 18:53:11 -0500

    Update Hangman.java

--- Java/Hangman.java
+++ Java/Hangman.java
@@ -8,9 +8,10 @@ public class Hangman
                             "thor", "wolverine", "spiderman"}
   private int mistakes = 6;
   private String hero;
-  private int numHeroes
+  private int numHeroes;
+  //private boolean answer;
   
-  \\ Constructor
+  // Constructor
   public Hangman()
   {
     Scanner scan = new Scanner(System.in);
@@ -29,13 +30,11 @@ public class Hangman
     temp = scan.nextln();
     guess = temp.toCharArray();
     */
-    temp = scan.nextln();
-    if(temp.length > 1)
+    guess = scan.nextln();
+    if(guess.length > 1)
       System.out.println("Sorry, Input is to long.");
     
-    if()
-
-
+    
   }
   
   public void choosenHero()
@@ -44,7 +43,6 @@ public class Hangman
     
     numHeroes = array.length - 1;
     hero = array[rand.nextInt(numHeroes)];
-
   }
   
   public String getHero()
@@ -52,10 +50,45 @@ public class Hangman
     return hero;
   }
 
-  public void mistakes()
+  public void mistakeMade()
   {
+    mistakes -= 1;
+    System.out.println("Sorry, You've the letter you choose is not found.");
+    String str = String.format("You have %d chances left.", getMistakes());
+    System.out.println(str);
     
+    //bool = false;
   }
+  
+  public int getMistakes()
+  {
+    return mistakes;
+  }
+  
+  /*
+  public boolean getBool()
+  {
+    return bool;
+  }
+  */
+  
+  // Takes users guess as parameter
+  // Method to check if the letter the user input is in the choosenHero
+  public void letterCheck(String g)
+  {
+    for(int i = 0; i < array.length; i++)
+    {
+      if(g == answer[i])
+      {
+        //g = answer[i];
+      }
+      else if(g != answer[i])
+      {
+        mistakeMade();
+      }
+    }// End of for loop
+    
+  }// End of letterCheck method
   
   public static void main(String[] args)
   {
