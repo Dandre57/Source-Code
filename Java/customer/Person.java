93955fdcdfb1f5e016504c51478083ea6cffee1e
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-12-05 12:26:32 -0500

    Create Person.java

---
+++ Java/customer/Person.java
@@ -0,0 +1,30 @@
+public class Person
+{
+  private String name;
+  private String address;
+  private String number;
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setAddress(String address) {
+    this.address = address;
+  }
+  
+  public String getAddress() {
+    return address;
+  }
+  
+  public void setNumber(String number) {
+    this.number = number;
+  }
+  
+  public String getNumber() {
+    return number;
+  }
+}
