380dffef7cd0f8f5d26f7d89b30bad767da89359
D'Andre Campbell <dandrecampbell5@gmail.com>
2017-10-17 01:01:40 -0400

    Update p2.c

--- C/p2.c
+++ C/p2.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	FILE *firstFile;	// Input File
 	FILE *secondFile;	// Output File
 	char fileContent;
 	int numOfLines = 0;
 	
+	if(argc != 3)
+	{
+		printf("Number of arguments is not correct.");
+		exit(0);
+	}
+	
 	// Reads characters from firstFile
-	firstFile = fopen("inputfile.txt", "r");
+	//firstFile = fopen("inputfile.txt", "r");
+	firstFile = fopen(argv[1] + ".txt", "r");  // Find out how to get this line to work
 	if(firstFile == NULL)
 	{
 		printf("File doees not exist or cannot be open. \n");
@@ -18,16 +25,18 @@ int main(void)
 	}
 	
 	// Adds words from firstFile to array (fileContent)
+	fileContent = fgetc(firstFile);
 	while(fileContent != EOF)
 	{
-		numOfLines++;
+		//numOfLines++;
+		fputc(fileContent, secondFile);
 		fileContent = fgetc(firstFile);
 		
 	}
 	
 	// Creates secondFile if it doesn't already exists and 
 	// adds fileContent from firstFile
-	secondFile = fopen(*secondFile , "w");
+	secondFile = fopen(argv[2], "w");
 	if(secondFile == NULL)
 	{
 		printf("Error. \n");
