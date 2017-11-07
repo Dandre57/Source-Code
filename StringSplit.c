b83ffed860860f393884409f7cf98906efae856b
D'Andre Campbell <dandrecampbell5@gmail.com>
2017-11-07 02:23:58 -0500

    Create StringSplit.c

---
+++ StringSplit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	char string[255];
+	char* command;
+	char* conversion;
+	char* text;
+	const char s[2] = " ";
+
+	//look up how to split string into different parts
+	
+	printf("Enter your command:"); fflush(stdout);
+	scanf("%s", &string);
+
+	//command = strtok(string, " ");
+	//conversion = strtok(NULL, " ");
+	//text = strtok(NULL, " ");
+	
+	//printf("%s %s %s", command, conversion, text);
+	/*
+	while(command != NULL)
+	{
+		printf("%s \n", command);
+		command = strtok(NULL, " ");
+	}
+	*/
+	for(command = strtok(string, " "); command != NULL; command = strtok(NULL, " ") )
+	{
+		printf("%s \n", command);
+	}
+	
+	return 0;
+}
