b9912bf5eda7a7ba416dc5a404839d3f3f814a98
D'Andre Campbell <dandrecampbell5@gmail.com>
2017-11-08 17:25:05 -0500

    Create p4.c

---
+++ C/p4.c
@@ -0,0 +1,50 @@
+/*
+Name:			D'Andre Campbell
+Unix Login ID:  dc959761@itsunix.albany.edu
+Lab Instructor: Chunpai Wang
+Lab: 			Monday 10:25 - 11:20
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void flagv();
+void flagf();
+void flagb();
+
+int main(int argc, char* argv[])
+{
+	FILE *firstFile;
+	FILE *secondFile;
+	char fileName[strlen(argv[1]) + 5];
+	char otherName[strlen(argv[1]) + 6];
+	char fileContent;
+	
+	/*
+	if(strcmp(argv[1], "-v") == 0)
+		flagv();
+	else if(strcmp(argv[1], "-f") == 0)
+		flagf();
+	else if(strcmp(argv[1], "-b") == 0)
+		flagb();
+	*/
+	
+	
+	return 0;
+}
+
+void flagv()
+{
+	
+}
+
+void flagf()
+{
+	
+}
+
+void flagb()
+{
+	
+}
