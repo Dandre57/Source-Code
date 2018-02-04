f104dad964eebc2d23a444d323b221f03b2081e4
D'Andre Campbell <dandrecampbell5@gmail.com>
2018-02-04 18:56:09 -0500

    Update CheckingBankAccount.java

--- Java/HW1/CheckingBankAccount.java
+++ Java/HW1/CheckingBankAccount.java
@@ -1,4 +1,19 @@
 public class CheckingBankAccount
 {
+  private double balance;
+  
+  public CheckingBankAccount(double balance)
+  {
+    this.balance = balance;
+  }
+  
+  public void deposit(double ammount)
+  {
+    balance += ammount;
+  }
 
+  public void withdraw(double ammount)
+  {
+    balance -= ammount;
+  }
 }
