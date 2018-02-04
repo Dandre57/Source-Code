5ab6ac789fac2c6ba3fe584ca3ff2a0f0fc3069e
D'Andre Campbell <dandrecampbell5@gmail.com>
2018-02-04 17:01:41 -0500

    Create HW1Problem2.java

---
+++ HW1Problem2.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class HW1Problem2 
+{
+	public boolean multiple(int arg1, int arg2)
+	{
+		if(arg1 % arg2 == 0)
+		{
+			System.out.println("It is divisible.");
+			return true;
+		}
+		else
+		{
+			System.out.println("There is a remainder.");
+			return false;
+		}		
+	}
+	
+	public int remainder(int arg)
+	{
+		int remainder = arg % 7;
+		return remainder;
+	}
+	
+	public double distance(double x1, double y1, double x2, double y2)
+	{
+		double difference;
+		
+		return difference;
+	}
+	
+	public void coinFlip()
+	{
+		
+	}
+}
