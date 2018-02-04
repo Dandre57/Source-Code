002930e40505ff0c29359defbde7e0bf722f8ea4
D'Andre Campbell <dandrecampbell5@gmail.com>
2018-02-04 18:34:25 -0500

    Create Address.java

---
+++ Java/HW1/Address.java
@@ -0,0 +1,57 @@
+public class Address
+{
+  private String street;
+  private String city;
+  private String state;
+  private String zip;
+
+  pubblic Address()
+  {
+  
+  }
+  
+  public void setStreet(String street)
+  {
+    this.street = street;
+  }
+  
+  public String getStreet()
+  {
+    return street;
+  }
+  
+  public void setCity(String city)
+  {
+  
+  }
+  
+  public String getCity()
+  {
+  
+  }
+  
+  public void setState()
+  {
+  
+  }
+  
+  public String getState()
+  {
+  
+  }
+  
+  public void setZip()
+  {
+  
+  }
+  
+  public String getZip()
+  {
+  
+  }
+  
+  public String toString()
+  {
+  
+  }
+}
