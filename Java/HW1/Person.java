64273c95a6c1257646ffe10eb038564ee62cf889
D'Andre Campbell <dandrecampbell5@gmail.com>
2018-02-04 19:00:31 -0500

    Create Person.java

---
+++ Java/HW1/Person.java
@@ -0,0 +1,12 @@
+public class Person
+{
+  private String firstName;
+  private String lastName;
+  private Address add = null;
+  private CheckingBankAccount cba = null;
+  
+  public Person()
+  {
+  
+  }
+}
