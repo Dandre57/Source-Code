f0e09f3ffa8cb638b969dfe76379252d7775121d
D'Andre Campbell <dandrecampbell5@gmail.com>
2018-02-04 19:03:35 -0500

    Update Person.java

--- Java/HW1/Person.java
+++ Java/HW1/Person.java
@@ -5,8 +5,29 @@ public class Person
   private Address add = null;
   private CheckingBankAccount cba = null;
   
-  public Person()
+  public Person(String firstName, String lastName)
   {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
   
+  public void setFirst(String firstName)
+  {
+    this.firstName = firstName;
+  }
+  
+  public String getFirst()
+  {
+    return firstName;
+  }
+  
+  public void setLast(String lastName)
+  {
+    this.lastName = lastName;
+  }
+  
+  public String getLast()
+  {
+    return lastName;
   }
 }
